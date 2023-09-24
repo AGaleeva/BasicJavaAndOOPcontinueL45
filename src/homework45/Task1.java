0b6c204abc3cec8910eb49fc1e456349faf3dcb1
AGaleea <98322303+AGaleea@users.noreply.github.com>
2023-09-24 19:23:02 +0200

    Task1.java has been modified

--- src/homework45/Task1.java
+++ src/homework45/Task1.java
@@ -1,7 +1,6 @@
 package homework45;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Task1 {
     /*
@@ -9,14 +8,28 @@ public class Task1 {
     */
 
     public static void main(String[] args) {
-        List<Integer> list = List.of(10, 8, 0, 4, 2, 5, 9, 1, 15, 7, 25);
+        List<Integer> list = new ArrayList<>(List.of(10, 8, 0, 4, -3, 2, 5, 9, 1, 15, 13, 7, 25));
         System.out.println(findPairComprisingNum(list, 10));
     }
-
+/*
     public static List<Integer> findPairComprisingNum(List<Integer> list, int n) {
         List<Integer> pairList = new ArrayList<>();
         if (list == null) return pairList;
         for (int i = 0, j = n; i < n / 2; i++, j = n - i) {
+            if (list.contains(i) && list.contains(j)) {
+                pairList.add(i);
+                pairList.add(j);
+            }
+        }
+        return pairList;
+    }*/
+
+    public static List<Integer> findPairComprisingNum(List<Integer> list, int n) {
+        List<Integer> pairList = new ArrayList<>();
+        if (list == null) return pairList;
+        Collections.sort(list, (i1, i2) -> Integer.compare(i1, i2));
+        System.out.println(list);
+        for (int i = list.get(0), j = n; i < n / 2; i++, j = n - i) {
             if (list.contains(i) && list.contains(j)) {
                 pairList.add(i);
                 pairList.add(j);
